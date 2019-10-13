75d88b85176934855b4dc605995510907406687c
Mudit Gupta <muditg317@gmail.com>
2019-10-13 18:35:15 -0400

    refactor stylesheets with sass

---
+++ dev_server.py
@@ -0,0 +1,7 @@
+import os
+os.chdir(r"C:\Users\mudit\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin")
+import sys
+sys.argv = ["dev_appserver.py",r"C:\Users\mudit\Desktop\Main_Drive\_Personal-Projects\personal-website\app.yaml"]
+__file__ = "dev_appserver.py"
+sys.path.append(r"C:\Users\mudit\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin")
+execfile("dev_appserver.py")
---
+++ sass_watch.bat
@@ -0,0 +1,1 @@
+sass --watch static/stylesheets/sass:static/stylesheets/css
